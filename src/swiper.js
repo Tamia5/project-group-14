6438571ab94db4f6b7bd279048f7bff6e11d067d
EugenL9x <l9xevgeniy@gmail.com>
2023-06-05 22:18:15 +0300

    swiper cho

--- src/swiper.js
+++ src/swiper.js
@@ -10,3 +10,16 @@ const swiper = new Swiper('.swiper', {
     autoplay: true,
   },
 });
+
+const swiper = new Swiper('.swiper-cho', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    autoplay: true,
+  },
+});
