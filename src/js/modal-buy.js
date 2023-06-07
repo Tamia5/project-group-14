7eb58ececa1b891f4cd8b34fb7c80ea59fffbe2d
Tamias <n.tamia5@icloud.com>
2023-06-07 16:25:42 +0300

    Update modal-buy.js

--- src/js/modal-buy.js
+++ src/js/modal-buy.js
@@ -20,7 +20,7 @@
 
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  // refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
